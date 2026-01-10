89e61d42ebd74c105a93ce8bc35e018d6fdc3b23
钰 刘 <18391128562@163.com>
2026-01-10 23:35:02 +0800

    渲染NDC坐标的学习

---
+++ 014_OpenGL_Exercise-1/assets/shaders/vertex.glsl
@@ -0,0 +1,16 @@
+#version 460 core  // 声明使用的 GLSL 版本为 460
+// 顶点着色器：处理每个顶点的坐标和颜色，输出顶点的变换位置和颜色信息
+// 输入变量
+layout (location = 0) in vec3 aPos;   // 定义输入变量 aPos，用于传入顶点坐标 (位置)，位置索引为 0（NDC坐标）
+layout (location = 1) in vec3 aColor; // 定义输入变量 aColor，用于传入顶点颜色，位置索引为 1
+
+// 输出变量
+out vec3 color;  // 定义输出变量 color，将传递到片段着色器中
+out vec3 position;
+
+void main()
+{
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); // 将输入的顶点位置 aPos 转换为四维向量并赋值给 gl_Position
+   color = aColor;  // 将输入的颜色 aColor 传递给输出变量 color
+   position=aPos;
+}
---
+++ 014_OpenGL_Exercise-1/wrapper/checkError.h
@@ -0,0 +1,10 @@
+#pragma once 
+//wrapper(包装器)
+//预编译宏(宏替换和预编译(解决发布时跳过开发期间的GL_CALL))
+#ifdef DEBUG
+#define GL_CALL(func) func;checkError();
+#else
+#define GL_CALL(func) func;
+#endif 
+
+void checkError();
